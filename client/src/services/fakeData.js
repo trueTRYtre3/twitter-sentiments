95f4ed813fb2116b40fd37a5c132683a1ded3984
Kyle Ortega <kyleortega253@gmail.com>
2021-08-10 19:14:06 -0700

    fake data to create ui

---
+++ client/src/services/fakeData.js
@@ -0,0 +1,14 @@
+const fakeData = [
+    {"Text":"","compound_score":"NEUTRAL"},
+    {"Text":"\"David Lee in the staff our batting 1000\n\nSIX STARS\n\" says cedric on Google, via _","compound_score":"NEUTRAL"},
+    {"Text":"Veterinarian Dr. Melissa Shapiro Discusses Book On Deaf &amp; Blind Dog Piglet \u2013 CBS\u00a0Seattle","compound_score":"NEGATIVE"},
+    {"Text":"Slog PM: UN Climate Change Report Is \"Code Red for Humanity,\" Seattle Children's Isn't Doing Enough to Combat Racism, Glo's Will Scoot Up to Capitol Hill Station - Slog - The Stranger","compound_score":"NEGATIVE"},
+    {"Text":"Clayton: Who and what stood out in Seahawks\u2019 scrimmage at Lumen Field \u2013 710 ESPN\u00a0Seattle","compound_score":"NEUTRAL"},
+    {"Text":"Clayton: Who and what stood out in Seahawks\u2019 scrimmage at Lumen Field \u2013 710 ESPN\u00a0Seattle","compound_score":"NEUTRAL"},
+    {"Text":"The City of Seattle goes incredibly hard for their 4x championship-winning WNBA team.","compound_score":"NEGATIVE"},
+    {"Text":"Another busy day at the COVID-19 Testing site as the \u201cdelta variant\u201d surges across the nation.\n\ud83d\udccdSeattle, WA \u2026","compound_score":"NEUTRAL"},
+    {"Text":"Confirmed: John Quenneville (F) | Seattle Kraken -&gt; ZSC |  NL","compound_score":"NEUTRAL"},
+    {"Text":"We honor with gratitude the land itself and the Duwamish Tribe as the first people of Seattle. \n\nThere are 29 federally re\u2026","compound_score":"POSITIVE"}
+]
+
+export default fakeData
